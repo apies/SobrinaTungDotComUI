fc518a89ba16e1c5852e0d80e1ab137e350bdcb6
Alan <alan.lee.pies@gmail.com>
2014-02-09 21:13:42 -0800

    fixed broken bower dependency

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-ui-router/angular-ui-router.js',
+      'app/bower_broken_components/angular-ui-router.js',
       'app/scripts/*.coffee',
       'app/scripts/**/*.coffee',
       'test/mock/**/*.coffee',
